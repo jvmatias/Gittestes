afe2264705279074db480e081317f316b2e2c74d
João Matias <joaosteam26@gmail.com>
2024-12-19 11:22:47 -0400

    useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MyButton from "./componentsPage/button";
 
 type Advice = {
@@ -8,20 +8,34 @@ type Advice = {
 const App: React.FC = () => {
   const [conselho, setAdvice] = useState<Advice>();
 
+  useEffect(() => {
+    loadAdvice();
+  }, []);
+
   const loadAdvice = async () => {
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
     const slip = data.slip;
-
+    console.log("troca de estado");
     setAdvice(slip);
   };
 
+  const handleAdvice = () => {
+    loadAdvice();
+  };
+
   return (
-    <div id="testeApi">
-      <MyButton onClick={loadAdvice} />
-      <div className="border rounded-md m-4 p-2 max-w-[45%]">
-        <p className="">{conselho?.advice}</p>
-      </div>
+    <div
+      id="testeApi"
+      className="flex flex-col justify-center gap-2 border rounded-md m-4 p-6 max-w-[45%]"
+    >
+      <p>{conselho?.advice}</p>
+      <button
+        className="bg-slate-600 cursor-pointer hover:bg-slate-400 hover:scale-105 rounded-md "
+        onClick={handleAdvice}
+      >
+        Trocar conselho
+      </button>
     </div>
   );
 };
--- src/componentsPage/button.tsx
+++ src/componentsPage/button.tsx
@@ -1,7 +1,7 @@
 const MyButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <button
-      className="bg-slate-400 border rounded-md p-2 m-4 hover:bg-slate-200 hover:scale-105 cursor-pointer transition ease-in-out delay-200"
+      className="bg-slate-400 border rounded-md my-2 hover:bg-slate-200 hover:scale-105 cursor-pointer transition ease-in-out delay-200"
       onClick={onClick}
     >
       clique aqui para um conselho
