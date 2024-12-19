d542af96c172512e895f38a91d89777ad7ca3d01
João Matias <joaosteam26@gmail.com>
2024-12-19 08:07:03 -0400

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import { ReactNode, useState } from "react"
-import MyButton from "./componentsPage/button"
+import { ReactNode, useState } from "react";
+import MyButton from "./componentsPage/button";
 
 type TitleProps = {
-  children: ReactNode
-  size?: "small" | "large"
-  color?: string
-}
+  children: ReactNode;
+  size?: "small" | "large";
+  color?: string;
+};
 
 const Title = ({ children, size, color }: TitleProps) => {
   return (
-    <p style={{
-      fontSize: size == `small` ? `1.5rem` : "3rem",
-      color: color
-    }}>{children}</p>
-  )
-}
+    <p
+      style={{
+        fontSize: size == `small` ? `1.5rem` : "3rem",
+        color: color,
+      }}
+    >
+      {children}
+    </p>
+  );
+};
 
 const App = () => {
-  const [count,setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
 
-  const handleClick = () =>{
-    setCount(count + 1)
-  }
-  
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <Title size="small" color="red">
         Hello World
       </Title>
 
-      <MyButton 
-      count={count}
-      onClick={handleClick}
-      />
+      <MyButton count={count} onClick={handleClick} />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/Pages/StatePratic.tsx
+++ src/Pages/StatePratic.tsx
@@ -1,31 +1,28 @@
 const User = {
-    name: "joao",
-    age: 20,
-    address: {
-        street: "violetas",
-        number: 192
-    }
-}
+  name: "joao",
+  age: 20,
+  address: {
+    street: "violetas",
+    number: 192,
+  },
+};
 
-console.log(pickProperty(User,"address"))
-
-
+console.log(pickProperty(User, "address"));
 
 type TypeVideo = {
-    title: string,
-    duration: number
-}
+  title: string;
+  duration: number;
+};
 
 const video: TypeVideo = {
-    title: "Typescript",
-    duration: 2000
-}
+  title: "Typescript",
+  duration: 2000,
+};
 
 function pickProperty<ObjectType>(obj: ObjectType, property: keyof ObjectType) {
-    return obj[property] 
+  return obj[property];
 }
 
-pickProperty(User, "name")
+pickProperty(User, "name");
 
-
-pickProperty(video, "title")
+pickProperty(video, "title");
