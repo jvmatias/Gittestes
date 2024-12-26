e299e26722f8353d1ded7e5a27ec91e64e88ed21
joao matias <victor.matias@topsapp.com.br>
2024-12-26 16:36:20 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
+import { useState } from "react";
 import useSWR from "swr";
-import { fetcher } from "./componentsPage/SWR";
 
 interface UserType {
   id: number;
-  name: string;
-  username: string;
-  phone: string;
+  title: string;
+  body: string;
+  userId: number;
 }
 
 const App = () => {
-  const { data } = useSWR<UserType>("users", fetcher);
-
-  console.log(data);
+  const { data, mutate } = useSWR<UserType[]>("/posts");
+  const [titulo, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   return (
     <div>
       <h2>Hello world</h2>
+      <div>
+        {data?.slice(0, 5).map((post) => {
+          return (
+            <div
+              className="grid grid-cols-1 bg-slate-200 m-2 p-2 rounded-md"
+              key={post.id}
+            >
+              <span>Titulo: {post.title}</span>
+              <span>Body: {post.body}</span>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="title">Insiria um titulo</label>
+        <input
+          className="border rounded-md"
+          type="text"
+          name="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="body">Insiria um body</label>
+        <input
+          className="border rounded-md"
+          type="text"
+          name="body"
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button className="border bg-slate-500 w-44 p-3 rounded-md">
+          Enviar novo post
+        </button>
+      </div>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { SWRConfig } from "swr";
+import { fetcher } from "./componentsPage/SWR.tsx";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <SWRConfig
+      value={{
+        fetcher,
+      }}
+    >
+      <App />
+    </SWRConfig>
+  </StrictMode>
+);
