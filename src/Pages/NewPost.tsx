a6df4a990694e226f88aab1f1fbb946a3214171c
joao matias <victor.matias@topsapp.com.br>
2024-12-27 13:56:16 -0400

    mais melhorias

--- src/Pages/NewPost.tsx
+++ src/Pages/NewPost.tsx
@@ -24,6 +24,11 @@ const addPost: React.FC = () => {
         const response = await postFetcher("/posts", newPost);
         if (response.status === 201) {
           setStatus(true);
+          setTimeout(() => {
+            setStatus(false);
+          }, 3000);
+          setIsSubmitted(false);
+          setNewPost({ title: "", body: "" });
         }
       }
     } catch (error) {
@@ -38,7 +43,7 @@ const addPost: React.FC = () => {
         <input
           name="name"
           onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-          placeholder=" "
+          value={newPost.title}
           className={`p-2 w-64 border rounded-md focus:ring-2
             ${
               isSubmitted && newPost.title.trim() === ""
@@ -52,32 +57,35 @@ const addPost: React.FC = () => {
         <label htmlFor="email">Corpo do post</label>
         <input
           name="email"
+          value={newPost.body}
           onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-          className={`p-2 border w-64 rounded-md ${
+          className={`p-2 border w-64 rounded-md shadow-md ${
             isSubmitted && newPost.body.trim() === ""
               ? "border-red-500 focus:outline-red-500"
               : "border-gray-500 focus:outline-blue-500"
           }`}
-          defaultValue=""
         />
-        {isSubmitted && newPost.title.trim() === "" && (
+        {isSubmitted && newPost.body.trim() === "" && (
           <span className="text-red-500">Preencha o formulario</span>
         )}
-        <input
-          className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
-          type="submit"
-          value="Enviar post"
-        />
-        <button
-          className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
-          onClick={() => navigate("/")}
-        >
-          Cancelar
-        </button>
+        <div className="flex flex-col gap-2 my-2">
+          <button
+            className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
+            type="submit"
+          >
+            Enviar post
+          </button>
+          <button
+            className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
+            onClick={() => navigate("/")}
+          >
+            Cancelar
+          </button>
+        </div>
+        {status && (
+          <span className="text-green-600 my-2d">Post enviado com sucesso</span>
+        )}
       </form>
-      {status && (
-        <span className="text-green-600">Post enviado com sucesso</span>
-      )}
     </div>
   );
 };
