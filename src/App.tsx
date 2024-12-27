e8b72843532167a874654bace0b5272734c98836
joao matias <victor.matias@topsapp.com.br>
2024-12-27 13:43:24 -0400

    muitas coisas

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App: React.FC = () => {
   const [openModal, setOpenModal] = useState(false);
   const [post, setPost] = useState("");
   const [body, setBody] = useState("");
+  const [screenY, setScreenY] = useState(0);
 
   const editPost = (id: number) => {
     const postMap = data?.map((post) => {
@@ -23,34 +24,52 @@ const App: React.FC = () => {
     return postMap;
   };
 
+  const pickClick = (e: any) => {
+    console.log(e);
+    if (e.target.value === "editarpost") {
+      e.pageY > 2000 ? setScreenY(e.pageY / 1.1) : setScreenY(e.pageY);
+    }
+  };
   return (
-    <div className="grid">
+    <div className="grid grid-cols-1">
       <div className="flex justify-between m-2">
         <h2>Blog Post</h2>
         <button
-          className="bg-red-300 rounded-md w-32 hover:bg-red-400 cursor-pointer "
+          className="bg-red-300 rounded-md w-32 hover:bg-red-400 cursor-pointer  shadow-md"
           onClick={() => navigate("/addPost")}
         >
           Add New Post
         </button>
       </div>
-      <div>
+      <div
+        onClick={(e) => pickClick(e)}
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-2 gap-2 sm:p-3 sm:gap-3 lg:p-4 lg:gap-4 xl:p-5 xl:gap-5"
+      >
         {openModal && (
           <div
-            className="absolute top-1/2 -translate-y-2/4
-          right-1/2 translate-x-2/4 bg-slate-500 w-[300px] min-h-[300px] z-10 rounded-md flex flex-col p-3 gap-3 "
+            className={`absolute -translate-y-2/4
+          right-1/2 translate-x-2/4 bg-slate-500 w-[300px] -h-[300px] sm:w-[550px] min-h-[350px] z-10 rounded-md flex flex-col p-3 gap-3 shadow-2xl drop-shadow-xl`}
+            style={{
+              top: `${screenY}px`,
+            }}
           >
-            <p>Editar usuario</p>
-            <label htmlFor="name">Titulo do post</label>
+            <p className="text-white">Editar usuario</p>
+            <label className="text-white" htmlFor="name">
+              Titulo do post
+            </label>
             <textarea
               name="name"
               value={post}
+              onChange={(e) => setPost(e.target.value)}
               className="bg-slate-400 p-2 border h-20 rounded-md"
             />
-            <label htmlFor="email">Corpo do post</label>
+            <label className="text-white" htmlFor="email">
+              Corpo do post
+            </label>
             <textarea
               name="email"
               value={body}
+              onChange={(e) => setBody(e.target.value)}
               className="bg-slate-400 p-2 border h-20 rounded-md"
             />
 
@@ -69,16 +88,18 @@ const App: React.FC = () => {
             </div>
           </div>
         )}
-        {data?.slice(0, 5).map((post) => {
+        {data?.slice(0, 16).map((post) => {
           return (
             <div
-              className="relative grid grid-cols-1 bg-slate-200 m-2 p-2 rounded-md"
+              className={`relative grid p-[inherit] gap-[inherit] bg-slate-100 rounded-md shadow-md
+                ${openModal ? "blur-sm" : "blur-none"}`}
               key={post.id}
             >
-              <span>Titulo: {post.title}</span>
-              <span>email: {post.body}</span>
+              <h2 className="text-xl first-letter:uppercase"> {post.title}</h2>
+              <p className="first-letter:uppercase"> {post.body}</p>
               <button
-                className="bg-red-300 rounded-md w-36 justify-self-end"
+                className="bg-red-300 rounded-md h-9 w-24 justify-self-end hover:bg-red-500 shadow-md"
+                value="editarpost"
                 onClick={() => {
                   editPost(post.id);
                   setOpenModal(true);
--- src/Pages/NewPost.tsx
+++ src/Pages/NewPost.tsx
@@ -14,23 +14,17 @@ const addPost: React.FC = () => {
     body: "",
   });
   const [status, setStatus] = useState(false);
-  const [error, setError] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    setIsSubmitted(true);
     try {
-      if (newPost.title || newPost.body !== "") {
+      if (newPost.title && newPost.body !== "") {
         const response = await postFetcher("/posts", newPost);
-
         if (response.status === 201) {
           setStatus(true);
-          setError(false);
         }
-
-        console.log(response);
-      } else {
-        setError(true);
       }
     } catch (error) {
       console.error(error);
@@ -38,28 +32,44 @@ const addPost: React.FC = () => {
   };
 
   return (
-    <div className="">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2 m-2">
+    <div className="bg-slate-100 gap-2 m-2 p-2 rounded-md shadow-md">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
         <label htmlFor="name">Titulo do post</label>
         <input
           name="name"
           onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-          className="p-2 border w-64 rounded-md"
+          placeholder=" "
+          className={`p-2 w-64 border rounded-md focus:ring-2
+            ${
+              isSubmitted && newPost.title.trim() === ""
+                ? "border-red-500 focus:outline-red-500"
+                : "border-gray-500 focus:outline-blue-500"
+            }`}
         />
+        {isSubmitted && newPost.title.trim() === "" && (
+          <span className="text-red-500">Preencha o formulario</span>
+        )}
         <label htmlFor="email">Corpo do post</label>
         <input
           name="email"
           onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-          className="p-2 border w-64 rounded-md"
+          className={`p-2 border w-64 rounded-md ${
+            isSubmitted && newPost.body.trim() === ""
+              ? "border-red-500 focus:outline-red-500"
+              : "border-gray-500 focus:outline-blue-500"
+          }`}
           defaultValue=""
         />
+        {isSubmitted && newPost.title.trim() === "" && (
+          <span className="text-red-500">Preencha o formulario</span>
+        )}
         <input
-          className="bg-red-300 rounded-md w-36 justify-self-end"
+          className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
           type="submit"
           value="Enviar post"
         />
         <button
-          className="bg-red-300 rounded-md w-36 justify-self-end"
+          className="bg-red-300 rounded-md w-36 justify-self-end hover:bg-red-500"
           onClick={() => navigate("/")}
         >
           Cancelar
@@ -68,7 +78,6 @@ const addPost: React.FC = () => {
       {status && (
         <span className="text-green-600">Post enviado com sucesso</span>
       )}
-      {error && <span className="text-red-600">Erro</span>}
     </div>
   );
 };
